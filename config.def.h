4c2136a61125564723d335df12cf822c9b9131c3
pryme-svg <edoc.www@gmail.com>
2021-04-02 10:09:50 -0700

    scroll with jk and ud

--- config.def.h
+++ config.def.h
@@ -201,6 +201,12 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+    { MODKEY,               XK_k,           kscrollup,      {.i =  1} },
+    { MODKEY,               XK_j,           kscrolldown,    {.i =  1} },
+    { MODKEY,               XK_Up,          kscrollup,      {.i =  1} },
+	{ MODKEY,               XK_Down,        kscrolldown,    {.i =  1} },
+	{ MODKEY,               XK_u,           kscrollup,      {.i = -1} },
+	{ MODKEY,               XK_d,           kscrolldown,    {.i = -1} },
 };
 
 /*
