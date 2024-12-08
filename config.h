dbee3a3f7e7e725435bf12a8a67d512d583d2c6d
wlmchen <wlmchen16@gmail.com>
2024-12-07 17:53:34 -0800

    Font Update

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack Nerd Font:pixelsize=18:antialias=true:autohint=true";
+static char *font = "HackNerdFont:pixelsize=18:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = { "JoyPixels:pixelsize=18:antialias=true:autohint=true", };
 
